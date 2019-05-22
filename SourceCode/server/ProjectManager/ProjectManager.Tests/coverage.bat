57e191a200b69a45a4b33a5822a4ad4e2b2938e3
fse-584598 <anupmail110@gmail.com>
2019-05-22 08:02:55 -0700

    Code Modified

--- SourceCode/server/ProjectManager/ProjectManager.Tests/coverage.bat
+++ SourceCode/server/ProjectManager/ProjectManager.Tests/coverage.bat
@@ -1,2 +1,2 @@
 
-C:\AnupMondal_584598\FSE_SBA\SourceCode\server\ProjectManager\packages\NUnit.ConsoleRunner.3.10.0\tools\nunit3-console.exe C:\AnupMondal_584598\FSE_SBA\SourceCode\server\ProjectManager\ProjectManager.Tests\bin\Debug\ProjectManager.Tests.dll
+C:\Users\Administrator\Desktop\Final\FSE_SBA_ProjectManager\SourceCode\server\ProjectManager\packages\NUnit.ConsoleRunner.3.10.0\tools\nunit3-console.exe C:\Users\Administrator\Desktop\Final\FSE_SBA_ProjectManager\SourceCode\server\ProjectManager\ProjectManager.Tests\bin\Debug\ProjectManager.Tests.dll
